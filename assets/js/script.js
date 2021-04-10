fa1b7ac7508bafcdd3780b7d5478beb4b0d88be5
ankitamestry27 <ankitamestry2706@gmail.com>
2021-04-10 20:24:48 +0530

    added loader functionality. added functions for next button and previous button click.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,17 +1,29 @@
 (function ($, w, d) {
+	// variable for total number of results should display on frontend
+	var num = 4,
+		$previous = $('.previous'),
+		$next = $('.next');
+
+	// show results on page load
 	fetch('./assets/data/airports.json')
 		.then(function (response) {
 			return response.json();
 		})
 		.then(function (data) {
-			console.log(data[0]);
 			var $results = $('.results'),
+				$loader = $('.loader'),
 				dataR,
-				ul;
+				ul,
+				dataCount = Object.keys(data).length,
+				$startNumElement = $('.res-numbers .start'),
+				$endNumElement = $('.res-numbers .end'),
+				$resTotal = $('.res-total');
 
-			for (var i = 0; i < 4; i++) {
+			$loader.hide();
+
+			for (var i = 0; i < dataCount; i++) {
 				dataR = data[i];
-				ul = `<li>
+				ul = `<li class="result">
 					<ul>
 						<li>` + dataR.name + `</li>
 						<li>` + dataR.icao + `</li>
@@ -22,7 +34,69 @@
 						<li>` + dataR.type + `</li>
 					</ul>
 				</li>`;
+
 				$results.append(ul);
 			}
+
+			for (var j = 1; j <= num; j++) {
+				$('.result:nth-of-type(' + j + ')').addClass('show');
+			}
+
+			$startNumElement.text(1);
+			$endNumElement.text(num);
+			$resTotal.text(dataCount);
 		});
+
+	// function for next button click
+	$next.click(function (e) {
+		e.preventDefault();
+		var $startNumElement = $('.res-numbers .start'),
+			startNum = parseInt($startNumElement.text()),
+			$endNumElement = $('.res-numbers .end'),
+			endNum = parseInt($endNumElement.text());
+
+		// hide all existing results
+		$('.result').removeClass('show');
+
+
+		if (startNum == 1) {
+			$previous.addClass('disable');
+		} else {
+			$previous.removeClass('disable');
+		}
+
+		// show results according to pagination after user clicked on next button
+		for (var i = endNum + 1; i < endNum + (num + 1); i++) {
+			$('.result:nth-of-type(' + i + ')').addClass('show');
+		}
+
+		$startNumElement.text(startNum + num);
+		$endNumElement.text(endNum + num);
+	});
+
+	// function for previous button click
+	$previous.click(function (e) {
+		e.preventDefault();
+		var $startNumElement = $('.res-numbers .start'),
+			startNum = parseInt($startNumElement.text()),
+			$endNumElement = $('.res-numbers .end'),
+			endNum = parseInt($endNumElement.text());
+
+		// hide all existing results
+		$('.result').removeClass('show');
+
+		if (startNum == 1 + num) {
+			$previous.addClass('disable');
+		} else {
+			$previous.removeClass('disable');
+		}
+
+		// show results according to pagination after user clicked on previous button
+		for (var i = startNum - (num + 1); i < startNum - 1; i++) {
+			$('.result:nth-of-type(' + i + ')').addClass('show');
+		}
+
+		$startNumElement.text(startNum - num);
+		$endNumElement.text(endNum - num);
+	});
 })(jQuery, window, document);
